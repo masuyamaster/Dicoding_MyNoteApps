81ab7426542e627a9ce0dc4bc5967902141a9889
masuyamaster <roziq@masuya.id>
2021-12-12 09:46:56 +0700

    latihan room

---
+++ app/src/main/java/ui/insert/NoteAddUpdateViewModel.kt
@@ -0,0 +1,19 @@
+package ui.insert
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import database.Note
+import repository.NoteRepository
+
+class NoteAddUpdateViewModel(application: Application) : ViewModel() {
+    private val mNoteRepository: NoteRepository = NoteRepository(application)
+    fun insert(note: Note) {
+        mNoteRepository.insert(note)
+    }
+    fun update(note: Note) {
+        mNoteRepository.update(note)
+    }
+    fun delete(note: Note) {
+        mNoteRepository.delete(note)
+    }
+}
